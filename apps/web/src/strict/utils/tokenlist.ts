0726cf1c9fc7d98d44690c765c7f765eda3e10c6
jasminenguyennn <jasmine@dhis2.org>
2023-09-18 19:08:47 +0100

    Default

--- apps/web/src/strict/utils/tokenlist.ts
+++ apps/web/src/strict/utils/tokenlist.ts
@@ -1,7 +1,9 @@
 import { trpcClient } from '@icecreamswap/backend'
 import { TokenList } from '@uniswap/token-lists'
+import defaultList from '../../../public/default.tokenList.json';
 
 export const getDefaultTokenList = async (): Promise<TokenList> => {
   // @ts-ignore
-  return trpcClient.token.defaultList.query()
+  return defaultList
+  // return trpcClient.token.defaultList.query()
 }
